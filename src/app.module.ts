5f2a9430c392a92ecfd3548dbf33e53eb03493ab
Nga To <19574802010125@vinhuni.edu.vn>
2026-05-08 14:00:35 +0700

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'ql_gplx',
+      entities: [],
+      synchronize: true,
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

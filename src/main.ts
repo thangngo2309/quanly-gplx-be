036c219bf1e39a1d8cf4becff293777d56150de6
Thang Ngo <hoangthang23091993@gmail.com>
2026-05-14 09:34:22 +0700

    update default port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3100);
 }
 bootstrap();

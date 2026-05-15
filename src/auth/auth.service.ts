c098bd9424617a80339b93522f4593085053f437
Nga To <19574802010125@vinhuni.edu.vn>
2026-05-15 18:46:54 +0700

    QG-12 fix login with status isActive=false

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,11 +13,11 @@ export class AuthService {
 
   async signIn(username: string, pass: string): Promise<{ access_token: string, refresh_token?: string }> {
     const user = await this.usersService.findByUsername(username);
-    if (!user) throw new UnauthorizedException();
+    if (!user) throw new UnauthorizedException("Tên đăng nhập hoặc mật khẩu không đúng");
 
     const isMatch = await bcrypt.compare(pass, user.password);
     if (!isMatch) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException("Tên đăng nhập hoặc mật khẩu không đúng");
     }
 
     const payload = { sub: user.user_id, username: user.username, fullname: user.fullname, role: user.role };
@@ -44,7 +44,7 @@ export class AuthService {
       });
 
       const user = await this.usersService.findByUsername(payload.username);
-      if (!user) throw new UnauthorizedException();
+      if (!user) throw new UnauthorizedException("Tên đăng nhập hoặc mật khẩu không đúng");
 
       const newPayload = {
         sub: user.user_id,
